a240cd406174daa1c028e1be6a4bd84922556223
Ronan Amicel <ronan.amicel@gmail.com>
2024-01-12 15:20:10 +0100

    Version initiale: un bot qui répond

---
+++ bot.py
@@ -0,0 +1,33 @@
+from pprint import pprint
+import os
+
+import discord
+from dotenv import load_dotenv
+
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f"{self.user} s’est connecté à Discord")
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content == "ping":
+            await message.channel.send("pong", reference=message)
+
+
+def main():
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    client = MyClient(intents=intents)
+    client.run(TOKEN)
+
+
+if __name__ == "__main__":
+    main()
